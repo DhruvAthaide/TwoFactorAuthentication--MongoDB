528b7dfb37f00e32f0c7c6b55e90f1ad9e7962a7
Dhruv Athaide <athaidedhruv@gmail.com>
2024-01-10 14:14:05 +0530

    Updated Routes and Forms

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.get("/",(req,res)=>{
     res.render("index");
 });
 
+app.get("/register",(req,res)=>{
+    res.render("register");
+});
+
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+
 app.post("/register",async (req,res)=>{
 
     const user = await User.findOne({username: req.body.username});
