6cabed9e3d77a3e19c4e1846a7ad88aa2241ae9c
Dhruv Athaide <athaidedhruv@gmail.com>
2024-01-10 14:41:28 +0530

    Integrated Passport.js with the Login

--- server.js
+++ server.js
@@ -1,46 +1,55 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const {connectMongoose,User} = require("./database.js")
-const ejs = require('ejs');
-const passport = require('passport');
-const {initializingPassport} = require("./passportConfig.js");
+const { connectMongoose, User } = require("./database.js");
+const ejs = require("ejs");
+const passport = require("passport");
+const { initializingPassport } = require("./passportConfig.js");
+const expressSession = require("express-session");
 
 connectMongoose();
 
 initializingPassport(passport);
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(
+  expressSession({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.get("/",(req,res)=>{
-    res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get("/register",(req,res)=>{
-    res.render("register");
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
-app.get("/login",(req,res)=>{
-    res.render("login");
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-app.post("/register",async (req,res)=>{
+app.post("/register", async (req, res) => {
+  const user = await User.findOne({ username: req.body.username });
 
-    const user = await User.findOne({username: req.body.username});
+  if (user) return res.status(400).send("User aldready exists!");
 
-    if(user) return res.status(400).send("User aldready exists!");
+  const newUser = await User.create(req.body);
 
-    const newUser = await User.create(req.body);
-
-    res.status(201).send(newUser);
+  res.status(201).send(newUser);
 });
-// app.post("/login",async (req,res)=>{
 
-// });
+app.post("/login",passport.authenticate("local",{failureRedirect:"/login"}),async (req,res)=>{
 
+});
 
-app.listen(3000, ()=>{
-    console.log("Listening on http://localhost:3000")
+app.listen(3000, () => {
+  console.log("Listening on http://localhost:3000");
 });
