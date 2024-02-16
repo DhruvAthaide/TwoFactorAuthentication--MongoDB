90305f22d05edcdf26a6d0e03921f21ffffaebbb
Dhruv Athaide <athaidedhruv@gmail.com>
2024-02-16 09:49:10 +0530

    Added Bcrpyt for better hashing

--- database.js
+++ database.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
-exports.connectMongoose = ()=>{
+exports.connectMongoose = () => {
     mongoose
-    .connect('mongodb://localhost:27017/passport')
-    .then(e=>console.log('Connected to MongoDB!'))
-    .catch((e)=>console.log(e));
+        .connect('mongodb://localhost:27017/passport', { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('Connected to MongoDB!'))
+        .catch((error) => console.error('Error connecting to MongoDB:', error));
 };
 
 const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true
+    username: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    password:String,
+    password: String,
 });
 
-exports.User = mongoose.model("User",userSchema);
+exports.User = mongoose.model("User", userSchema);
--- passportConfig.js
+++ passportConfig.js
@@ -1,44 +1,46 @@
 const localStrategy = require('passport-local').Strategy;
-const {User} = require('./database.js');
+const bcrypt = require('bcrypt');
+const { User } = require('./database.js');
 
-exports.initializingPassport = (passport)=>{
+exports.initializingPassport = (passport) => {
     passport.use(new localStrategy({
-        usernameField:'email',
-        passwordField:'password'
+        usernameField: 'email', // Assuming you have an input with name="email" in your form
+        passwordField: 'password',
     },
-    async function (username,password,done){
-        try{
+        async function (username, password, done) {
+            try {
+                const user = await User.findOne({ username });
 
-            const user = await User.findOne({username});
-    
-            if(!user) return done(null, false);
-    
-            if(user.password !== password) return done(null, false);
-            
-            return done(null, user);
-        }catch (error){
-            return done(error, false);
-        }
-    })
+                if (!user) return done(null, false);
+
+                const isPasswordValid = await bcrypt.compare(password, user.password);
+
+                if (!isPasswordValid) return done(null, false);
+
+                return done(null, user);
+            } catch (error) {
+                return done(error, false);
+            }
+        })
     );
 
-    passport.serializeUser((user,done)=>{
-        done(null,user.id);
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
     });
 
-    passport.deserializeUser(async(id,done)=>{
-        try{
-            const user =  await User.findById(id);
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await User.findById(id);
 
-            done(null,user);
-        }catch(error){
-            done(error,false);
+            done(null, user);
+        } catch (error) {
+            done(error, false);
         }
     });
 };
 
-exports.isAuthenticated = (req,res,next)=>{
-    if(req.user) return next();
+exports.isAuthenticated = (req, res, next) => {
+    if (req.user) return next();
 
     res.redirect("/login");
 };
